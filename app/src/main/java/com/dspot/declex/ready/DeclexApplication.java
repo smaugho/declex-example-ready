78770565c96c96533b1af17bd254dff45d8e35ae
Smaugh <arivero.smaugh@gmail.com>
2017-03-08 11:17:32 +0100

    Updating with the last version of DecleX

--- app/src/main/java/com/dspot/declex/ready/DeclexApplication.java
+++ app/src/main/java/com/dspot/declex/ready/DeclexApplication.java
@@ -6,10 +6,6 @@ import com.dspot.declex.api.localdb.UseLocalDB;
 
 import org.androidannotations.annotations.EApplication;
 
-/**
- * Created by Adrian on 07.11.2016.
- */
-
 @EApplication
 @UseLocalDB
 public class DeclexApplication extends Application {
--- app/src/main/java/com/dspot/declex/ready/MainActivity.java
+++ app/src/main/java/com/dspot/declex/ready/MainActivity.java
@@ -1,12 +1,19 @@
 package com.dspot.declex.ready;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    @Click
+    void declexLink() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/smaugho/declex"));
+        startActivity(intent);
+    }
 
 }
