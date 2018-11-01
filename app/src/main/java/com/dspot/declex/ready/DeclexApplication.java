93801786e582c059aa7406250fc6ed093fd44a3b
Adrian Rivero <arivero.smaugh@gmail.com>
2018-11-01 22:15:28 +0100

    Configure DecleX 2.0 (alpha)

--- app/src/main/java/com/dspot/declex/ready/DeclexApplication.java
+++ app/src/main/java/com/dspot/declex/ready/DeclexApplication.java
@@ -2,11 +2,8 @@ package com.dspot.declex.ready;
 
 import android.app.Application;
 
-import com.dspot.declex.api.localdb.UseLocalDB;
-
 import org.androidannotations.annotations.EApplication;
 
 @EApplication
-@UseLocalDB
 public class DeclexApplication extends Application {
 }
